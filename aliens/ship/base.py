25dc39de0f13891cd17730310f96c0b7c8719092
pp <p2008@op.pl>
2020-12-26 23:36:53 +0100

    Ship steering, speed and movement boundaries

--- aliens/ship/base.py
+++ aliens/ship/base.py
@@ -20,6 +20,25 @@ class BaseShip:
         # New ships will be shown on the bottom of the screen
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
+        self.center = float(self.rect.centerx)
+
+        # Attributes corresponding to ship movement
+        self.moving_right = False
+        self.moving_left = False
+        self.ship_speed_factor = 1.5
+
+    def update(self):
+        """
+        Update ship placement base on moving attributes
+        """
+
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.center += self.ship_speed_factor
+
+        if self.moving_left and self.rect.left > 0:
+            self.center -= self.ship_speed_factor
+
+        self.rect.centerx = self.center
 
     def blit_me(self):
         """
